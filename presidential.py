9313933998cfda793eef1984b4d7e49692ae542d
Dr. Nick <nicholas.dahm@gmail.com>
2017-01-21 19:42:42 -0500

    webcam looping and squirrel overlay

--- presidential.py
+++ presidential.py
@@ -19,7 +19,7 @@ cv2.imshow('squirrel gray', sq_gray)
 
 sq_gray_mask = sq_gray > 0
 sq_mask = np.array([[[v, v, v] for v in row] for row in sq_gray_mask])
-cv2.waitKey()
+#cv2.waitKey()
 
 
 
@@ -33,5 +33,27 @@ sq2 = cv2.cvtColor(sq2, cv2.COLOR_RGB2BGR)
 # cv2 -> PIL
 pilsq2 = cv2.cvtColor(sq, cv2.COLOR_BGR2RGB)
 pilsq2 = Image.fromarray(pilsq2)
+
+
+
+
+
+# Webcam viewing
+print 'starting webcam...'
+cam = cv2.VideoCapture(0)
+ret, frame = cam.read()
+assert ret
+print 'webcam res:', frame.shape
+
+while True:
+    ret, frame = cam.read()
+
+    # overlay the squirrel
+    np.copyto(frame[-sq.shape[0]:, 0:sq.shape[1], :], sq, where=sq_mask)
+
+    cv2.imshow('cam', frame)
+    key = cv2.waitKey(1)
+    if key != -1:
+        break
 
 
